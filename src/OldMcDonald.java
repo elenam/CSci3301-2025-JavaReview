ba8deeb22d6df4fb01694448bd79880fb3943cb6
Elena M <elenam@morris.umn.edu>
2025-01-15 12:25:25 -0600

    added an example for Liskov principle discussion

--- src/OldMcDonald.java
+++ src/OldMcDonald.java
@@ -1,11 +1,14 @@
 
 public class OldMcDonald {
-
+/*
+ * Printing a simplified version of old McDonald's song using interfaces
+ */
 	public static void main(String[] args) {
+		// Task: Make the program general, without having to construct 
+		// each verse manually. 
 		Animal animal1 = new Pig();
 		Animal animal2 = new Duck();
 		
-		// Verse 1:
 		String farmLine = "Old MacDonald had a farm. E-I-E-I-O.\n";
 		String animalLine1 = "And on that farm he had a " + animal1.getName() + ". E-I-E-I-O.\n";
 		String soundLines1 = "An " + animal1.getSound() + " " 
