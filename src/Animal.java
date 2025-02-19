78fcebc9c38989979e0d56280076636e323f15df
Elena M <elenam@morris.umn.edu>
2025-02-19 12:18:34 -0600

    added examples of reflection

--- src/Animal.java
+++ src/Animal.java
@@ -1,14 +1,25 @@
 
-public interface Animal {
+// A class for reflection example
+public class Animal {
+	private String name;
+	private double weight;
 	
-	/*
-	 * The method returns the animal's name
-	 */
-	public String getName();
+	public Animal(String name, double weight) {
+		this.name = name;
+		if (validWeight(weight)) {
+			this.weight = weight;
+		}
+	}
 	
-	/*
-	 * The method returns the animal's sound
-	 */
-	public String getSound();
-
+	private boolean validWeight(double weight) {
+		return (weight > 0);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getWeight() {
+		return weight;
+	}
 }
